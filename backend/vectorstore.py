cac45058f8eabe2bd64645a43ef4154a62ad046e
R1tMah <rimahapatra7@gmail.com>
2026-01-30 04:58:55 -0500

    changes

--- backend/vectorstore.py
+++ backend/vectorstore.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 from langchain_community.document_loaders import TextLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain_chroma import Chroma
 
